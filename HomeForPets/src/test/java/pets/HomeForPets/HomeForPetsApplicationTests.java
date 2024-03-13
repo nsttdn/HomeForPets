4e19714a803a85e054d05ecb82cb05795a044572
nsttdn <dejnekan@gmail.com>
2024-03-13 18:38:28 +0200

    Initial commit

---
+++ HomeForPets/src/test/java/pets/HomeForPets/HomeForPetsApplicationTests.java
@@ -0,0 +1,13 @@
+package pets.HomeForPets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeForPetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
